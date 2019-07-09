ada360bc1dea0bb019486192bc94510add291904
이석원 <blackmonkey9256@gmail.com>
2019-07-09 10:50:56 +0900

    firebase를 통한 인증

---
+++ src/helpers/firebaseConfig.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyAH-cHShN1MCm-Mu90BLTARB4qGW73d9n8",
+    authDomain: "authtest-209ba.firebaseapp.com",
+    databaseURL: "https://authtest-209ba.firebaseio.com",
+    projectId: "authtest-209ba",
+    storageBucket: "",
+    messagingSenderId: "779811635258",
+    appId: "1:779811635258:web:a454746d426f40fa"
+};
---
+++ src/main.js
@@ -0,0 +1,26 @@
+import Vue from 'vue';
+import App from './App.vue';
+import firebase from 'firebase';
+import firebaseui from 'firebaseui';
+import VueRouter from 'vue-router';
+import router from './router';
+
+import {firebaseConfig} from './helpers/firebaseConfig';
+
+Vue.use(VueRouter);
+
+new Vue({
+  router,
+  created(){
+    firebase.initializeApp(firebaseConfig);
+    firebase.auth().onAuthStateChanged((user) => {
+      if(user){
+        this.$router.push('/success');
+      }else{
+        this.$router.push('/auth');
+      }
+    });
+  },
+  el: '#app',
+  render: h => h(App)
+})
